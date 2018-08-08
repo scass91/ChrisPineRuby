47efe26325538414c9b273fd0971ec1fb20eded8
Simon Cass <scass1991@gmail.com>
2018-08-09 00:04:42 +0100

    initial commit - chapter 1

---
+++ numbers.rb
@@ -0,0 +1,21 @@
+=begin
+August 9th 2018
+Chris Pine Ruby
+Chapter 1 - numbers
+=end
+
+#A Few Things to Try
+#Write a program which tells you:
+#how many hours are in a year?
+puts 24*365
+#how many minutes are in a decade?
+puts 60*24*3650
+#how many seconds old are you?
+puts 27.5 * 365 * 24 * 60 * 60
+#how many chocolates do you hope to eat in your life?
+#Warning: This part of the program could take a while to compute!
+puts "All of it"
+
+#Here's a tougher question:
+#If I am 1298 million seconds old, how old am I?
+puts 1298000000 / 60 / 60 / 24 / 365
